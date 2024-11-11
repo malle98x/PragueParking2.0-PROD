836eca73a8d5d31729c8cc6da2a8f6068d02513d
malle98x <134820922+malle98x@users.noreply.github.com>
2024-11-11 19:14:08 +0100

    Rensa inbäddade repository och lägga till våra tester

---
+++ PragueParking.Tests/ParkeringsHusTest.cs
@@ -0,0 +1,35 @@
+﻿// ParkeringsHusTest.cs
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PragueParking.Models;
+
+namespace PragueParking.Test
+{
+    [TestClass]
+    public class ParkeringsHusTests
+    {
+        [TestMethod]
+        public void ParkeraFordon_SkaReturneraTrue_NarDetFinnsPlats()
+        {
+            var parkeringshus = new ParkeringsHus(antalPlatser: 10, platsStorlek: 4);
+            var bil = new Bil("ABC123");
+
+            bool resultat = parkeringshus.ParkeraFordon(bil);
+
+            Assert.IsTrue(resultat);
+        }
+
+        [TestMethod]
+        public void TaBortFordon_SkaReturneraTrue_NarFordonFinns()
+        {
+            var parkeringshus = new ParkeringsHus(antalPlatser: 10, platsStorlek: 4);
+            var bil = new Bil("JFW556");
+            parkeringshus.ParkeraFordon(bil);
+
+            bool resultat = parkeringshus.TaBortFordon("JFW556", out Fordon? borttagetFordon);
+
+            Assert.IsTrue(resultat);
+            Assert.IsNotNull(borttagetFordon);
+            Assert.AreEqual("JFW556", borttagetFordon.RegistreringsNummer);
+        }
+    }
+}
---
+++ PragueParking.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace PragueParking.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
